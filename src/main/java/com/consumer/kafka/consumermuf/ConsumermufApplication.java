35075765e2547f258aa6a05bc015b99214e2b492
Nur Yudha <nur.ramadhan@muf.co.id>
2024-05-17 19:57:19 +0700

    first commit

---
+++ src/main/java/com/consumer/kafka/consumermuf/ConsumermufApplication.java
@@ -0,0 +1,13 @@
+package com.consumer.kafka.consumermuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumermufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumermufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumer/kafka/consumermuf/ConsumermufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer.kafka.consumermuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumermufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
